6dded8e57b3a0949ab5766baa8863578ad50c3f0
reuben-philip <reubenaphilip@gmail.com>
2025-03-18 12:48:28 -0700

    Fix: disbaled pretty printing

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ const express = require ('express')
 const app = express()
 const port = 80
 
+app.set('json spaces', 0);
+
 app.get('/', (req,res) => {
     const data = {
         message:"My name is Reuben",
