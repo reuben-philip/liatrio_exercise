b22d263cd9671487f8f2b10a76c4ee449962601f
reuben-philip <reubenaphilip@gmail.com>
2025-03-23 12:48:11 -0700

    updated api/index.js and added comments to the file

--- api/index.js
+++ api/index.js
@@ -1,18 +1,23 @@
+//express library 
 const express = require ('express')
 const app = express()
+
+//set the port to use the environment variable or port 80
 const port =process.env.PORT || 80
 
-
+//route for HTTP GET requests to the root URL
 app.get('/', (req,res) => {
-    res.json({
+    const data = {
         message:"My name is Reuben",
-        timestamp: Date.now(),
-        test:"test",
-        color: "red"
-    });
+        timestamp: Date.now()
+    };
+
+    //send object as JSON to the client
+    res.json(data)
 
 });
 
+//start the server and listen to the specified port 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
